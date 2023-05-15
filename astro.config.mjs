97d5d9ac9241726f6bf833048e2493e9113b05bc
rstegmann-arch <rstegmann@immap.org>
2023-05-14 23:43:25 -0400

    added theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    server:{
-        port: 4000,
-        host: true
-    }
+  server: {
+    port: 4000,
+    host: true
+  },
+  integrations: [preact()]
 });
